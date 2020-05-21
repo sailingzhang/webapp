d859cb56e431145ebefb005f6ed22a30b609e257
sailingzhang <aa582885848@outlook.com>
2020-05-21 18:28:49 +0800

    try opencv js

---
+++ cactus/src/components/typetest.d.ts
@@ -0,0 +1,11 @@
+// declare module MyMo { 
+//     // export class Calc { 
+//     //    doSum(limit:number) : number; 
+//     // }
+//    export function typet(value: string): string;
+//  }
+
+export declare function typet(value: string): string;
+
+// export function typet(value: string): string;
+
---
+++ cactus/src/components/typetest.js
@@ -0,0 +1,5 @@
+
+export function typet(value){
+    console.log("in js,value=%s",value);
+    return value;
+}
