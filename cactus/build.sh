b8477f9dd1c24b5ea1714a3c4b7fd82ed7411266
sailingzhang <sailingzhang@aa582885848@outlook.com>
2021-02-02 17:46:49 +0800

    for signal docker

--- cactus/build.sh
+++ cactus/build.sh
@@ -6,8 +6,8 @@ app_config_dir=/app_config/www/cactus
 mkdir -p ${appdir}
 mkdir -p ${app_config_dir}
 
-cp -rf  css ${app_config_dir}
-cp -rf  dist  ${app_config_dir}
-cp -rf  *.html  ${app_config_dir}
-cp -f  ../mime.types   ${app_config_dir}
-cp -f  ../nginx_static.conf    ${app_config_dir}
+sudo cp -rf  css ${app_config_dir}
+sudo cp -rf  dist  ${app_config_dir}
+sudo cp -rf  *.html  ${app_config_dir}
+sudo cp -f  ../mime.types   ${app_config_dir}
+sudo cp -f  ../nginx_static.conf    ${app_config_dir}
